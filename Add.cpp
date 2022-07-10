8ed9ae995a99307c873c7563b1ca1406bb36717f
Pratik Kashinath Darange <pratikpd2391@gmail.com>
2022-07-10 18:29:49 +0530

    adding Hello.cpp

---
+++ Add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 9;
+   
+    int sum = a+b;
+
+    cout<<"The sum of two numbers are "<<sum;
+
+    return 0;
+}
