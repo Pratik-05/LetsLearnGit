9c901bf1ad6f12d36d812eb8fb5bd54766b596b4
Pratik Kashinath Darange <pratikpd2391@gmail.com>
2022-07-10 19:50:40 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 9;
+   
+    int mul = a*b;
+
+    cout<<"The multiplication of two numbers are "<<mul;
+
+    return 0;
+}
